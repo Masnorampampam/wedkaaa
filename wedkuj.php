d9c2cf48ecd62160ec0aedb6ef6e436b6e8e7039
Mateusz Kreft <mateuszkreft6@gmail.com>
2023-09-18 11:36:16 +0200

    wedkowanie

---
+++ wedkuj.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+	<meta charset="utf-8" />
+	<title>Wędkowanie</title>
+	<link rel="stylesheet" href="styl_1.css" />
+</head>
+<body>
+	<header>
+		<h1>Portal dla wędkarzy</h1>
+	</header>
+	<main>
+		<section id="lewy1">
+			<h3>Ryby zamieszkujące rzeki</h3>
+			<ol>
+                <li>Szczupak pływa w rzece Warta-Obrzycko. Wielkopolskie</li>
+                <li>Leszcz pływa w rzece Przemsza k. Okradzinowa. Slaskie</li>
+			</ol>
+		</section>
+		<section id="prawy">
+			<img src="ryba1.jpg" alt="Sum" /><br/>
+			<a href="kwerendy.txt">Pobierz kwerendy</a>
+		</section>
+		<section id="lewy2">
+			<h3>Ryby drapieżne naszych wód</h3>
+			<table>
+				<tr>
+					<th>L.p.</th>
+					<th>Gatunek</th>
+					<th>Występowanie</th>
+				</tr>
+                <tr>
+                    <td>1</td>
+                    <td>Szczupak</td>
+                    <td>stawy, rzeki</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Sandacz</td>
+                    <td>stawy, jeziora, rzeki</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Okon</td>
+                    <td>rzeki</td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>Sum</td>
+                    <td>jeziora, rzeki</td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td>Dorsz</td>
+                    <td>morza, oceany</td>
+                </tr>
+
+			</table>
+		</section>
+	</main>
+	<footer>
+		<p>Stronę wykonał: Mateusz Kreft 5hp
+	</footer>
+</body>
+</html>
